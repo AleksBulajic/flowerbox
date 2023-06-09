12437020231da1290895d05fc71141598f612db2
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-06-09 09:25:26 -0400

    little fix

--- src/Components/SmallImg.js
+++ src/Components/SmallImg.js
@@ -2,6 +2,8 @@ import React from "react";
 
 function SmallImg({ data }) {
   return (
+
+
     <div className="small">
       <img src={data.image} alt="flower" />
       <div style={{ display: "flex", flexDirection: "row", padding: 15 }}>
@@ -12,7 +14,6 @@ function SmallImg({ data }) {
           ${data.price}
         </p>
       </div>
-
       <div  className="map"style={{ display: "flex", flexDirection: "row" }}>
         {data.tags.map((tag) => (
           <p className="tags" >
